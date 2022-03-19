cec7491556df0bc5827f170bdb9b2700cef98a86
INFORMATIC0 <86555103+INFORMATIC0@users.noreply.github.com>
2022-03-19 16:56:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,4 @@
     $cadena = file_get_contents("files/"+$_GET["name"]+".json");
     $cadena .= $_GET("new");
      file_put_contents("files/"+$_GET("name")+".json", $cadena);
-     echo file_get_contents($uri);
-     header('Content-Type: application/json; charset=utf-8');
 ?>
